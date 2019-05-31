e13ec9470048c0be725118e27f993b55ff8d5b1f
joaorincon <joao.rincon@correounivalle.edu.co>
2019-05-31 11:37:34 -0500

    Adicionando ejemplo de archivo de cabecera

---
+++ 2019_05_31/principal.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "entero.h"
+
+int main(int argc, char** argv) {
+  entero a,b;
+
+  a = 5;
+  b = 6;
+  printf("la suma de %d + %d es %d\n",a,b,suma(a,b));
+  //printf("la division de %d + %d es %f\n",a,b,(float)division(a,b));
+}
+
