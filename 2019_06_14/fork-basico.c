a3f561b7b52453ee8ef637104decc0148d4efd8e
Invitado <invitado@s5pc25.eisc.univalle.edu.co>
2019-06-14 11:55:09 -0500

    adicionando archivos fuente

---
+++ 2019_06_14/fork-basico.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+Este programa en C debe imprimir lo siguiente:
+
+Soy el hijo y mi identificador es 34567
+Soy el padre, mi identificador es 34566 y el de mi hijo es 34567
+
+Hacer uso de la función 'getpid()'
+*/
+
+int main(int argc, char** argv){
+	pid_t pid;
+
+	pid = fork();
+
+	if(pid==0){
+		printf("Soy el hijo y mi id es: %d\n",(int) getpid());
+	}
+	else{
+		printf("Soy el padre, mi id es %d\n y mi hijo es %d\n",getpid(), pid);
+	}
+}
