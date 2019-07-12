122f1504b30f093ccc14bdd045d30f350fc7f9ab
Invitado <invitado@s5pc26.eisc.univalle.edu.co>
2019-07-12 11:36:58 -0500

    temporal_var_2.c

---
+++ 2019_07_12/temporal_var_2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int suma(int *x, int y){
+  int z;
+  *x = *x + y;
+  z = *x;
+  return z;
+}
+
+int main(int argc, char** argv){
+  int x1=4,y1=5;
+  printf("Suma; %d\n", suma(&x1,y1));
+  printf("x=%d\n", x1);
+  return 0;
+}
